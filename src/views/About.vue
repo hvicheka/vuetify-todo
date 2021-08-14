f50f9f0c121c1d99faec36556859d80f41c811a7
Hok Vicheka <hvicheka.it@gmail.com>
2021-08-14 21:33:38 +0700

    Todo Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vueify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad aliquid, assumenda cumque deserunt eaque expedita
+      itaque molestiae nemo nesciunt quaerat rem similique. Beatae fugiat harum quos ratione repellendus soluta?
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="home pa-6">
+    <v-text-field
+        v-model="newTaskTitle"
+        @click:append="addTask"
+        @keyup.enter="addTask"
+        class="mb-3"
+        outlined
+        label="New Task"
+        clearable
+        hide-details
+        append-icon="mdi-plus"
+    ></v-text-field>
+    <v-list
+        v-if="tasks.length"
+        class="p-0"
+        flat
+    >
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item @click="doneTask(task.id)" :class="{'blue lighten-5' : task.done}">
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title :class="{'text-decoration-line-through': task.done}">
+                {{ task.title }}
+              </v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn
+                  @click.stop="deleteTask(task.id)"
+                  icon
+              >
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+    <div v-else class="no-task">
+      <v-icon size="100" color="primary">
+        mdi-check
+      </v-icon>
+      <div class="text-h5 primary--text">No Tasks</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      newTaskTitle: '',
+      tasks: [
+        // {
+        //   id: 1,
+        //   title: "Wake up",
+        //   done: true
+        // },
+        // {
+        //   id: 2,
+        //   title: "Get Bananas",
+        //   done: false
+        // },
+        // {
+        //   id: 3,
+        //   title: "Eat Bananas",
+        //   done: false
+        // }
+      ]
+    }
+  },
+  methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    },
+    doneTask(id) {
+      let task = this.tasks.find(task => task.id === id)
+      task.done = !task.done
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.no-task {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+</style>

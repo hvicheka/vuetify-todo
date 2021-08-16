84d3293e7b6e2d268fd663eea934ecaa9157eec2
vichekaphkaymedia <vicheka.phkaymedia@gmail.com>
2021-08-16 12:59:39 +0700

    ListTask Component

---
+++ src/components/Todo/ListTask.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-list
+      class="p-0"
+      flat
+  >
+    <div v-for="task in $store.state.tasks" :key="task.id">
+      <v-list-item @click="$store.commit('doneTask',task.id)" :class="{'blue lighten-5' : task.done}">
+        <template v-slot:default>
+          <v-list-item-action>
+            <v-checkbox :input-value="task.done"></v-checkbox>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title :class="{'text-decoration-line-through': task.done}">
+              {{ task.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn
+                @click.stop="$store.commit('deleteTask',task.id)"
+                icon
+            >
+              <v-icon color="primary lighten-1">mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </template>
+      </v-list-item>
+      <v-divider></v-divider>
+    </div>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: "ListTask"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,35 +1,7 @@
 <template>
   <div class="home pa-6">
     <new-task></new-task>
-    <v-list
-        v-if="$store.state.tasks.length"
-        class="p-0"
-        flat
-    >
-      <div v-for="task in $store.state.tasks" :key="task.id">
-        <v-list-item @click="$store.commit('doneTask',task.id)" :class="{'blue lighten-5' : task.done}">
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title :class="{'text-decoration-line-through': task.done}">
-                {{ task.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn
-                  @click.stop="$store.commit('deleteTask',task.id)"
-                  icon
-              >
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
+    <list-task  v-if="$store.state.tasks.length"></list-task>
     <div v-else class="no-task">
       <v-icon size="100" color="primary">
         mdi-check
@@ -41,10 +13,14 @@
 
 <script>
 import NewTask from "../components/Todo/NewTask";
+import ListTask from "../components/Todo/ListTask";
 
 export default {
   name: 'Home',
-  components: {NewTask}
+  components: {
+    NewTask,
+    ListTask
+  }
 }
 </script>
 

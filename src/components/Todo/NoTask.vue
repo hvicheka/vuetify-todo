b2d2e2fbf2fbbd37b3ee3bcf2427f766b914f34b
vichekaphkaymedia <vicheka.phkaymedia@gmail.com>
2021-08-16 13:15:06 +0700

    NoTask Component

---
+++ src/components/Todo/NoTask.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="no-task">
+    <v-icon size="100" color="primary">
+      mdi-check
+    </v-icon>
+    <div class="text-h5 primary--text">No Tasks</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoTask"
+}
+</script>
+
+<style lang="css">
+.no-task {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,34 +1,24 @@
 <template>
   <div class="home pa-6">
     <new-task></new-task>
-    <list-task  v-if="$store.state.tasks.length"></list-task>
-    <div v-else class="no-task">
-      <v-icon size="100" color="primary">
-        mdi-check
-      </v-icon>
-      <div class="text-h5 primary--text">No Tasks</div>
-    </div>
+    <list-task v-if="$store.state.tasks.length"></list-task>
+    <no-task v-else></no-task>
   </div>
 </template>
 
 <script>
 import NewTask from "../components/Todo/NewTask";
 import ListTask from "../components/Todo/ListTask";
+import NoTask from "../components/Todo/NoTask";
 
 export default {
   name: 'Home',
   components: {
     NewTask,
-    ListTask
+    ListTask,
+    NoTask
   }
 }
 </script>
 
-<style lang="css">
-.no-task {
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  position: absolute;
-}
-</style>
+

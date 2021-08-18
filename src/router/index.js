102cb0c14e6beb3a2a54c23eb57247d2da8e807b
Hok Vicheka <hvicheka.it@gmail.com>
2021-08-18 21:29:53 +0700

    Remove # from URL

--- src/router/index.js
+++ src/router/index.js
@@ -17,10 +17,11 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    },
 ]
 
 const router = new VueRouter({
+    mode: 'history',
     routes
 })
 

52e311f6ca5891cd08ccd5637273ce2702c78e2d
vichekaphkaymedia <vicheka.phkaymedia@gmail.com>
2021-08-20 18:26:25 +0700

    add

--- src/store/index.js
+++ src/store/index.js
@@ -72,6 +72,10 @@ export default new Vuex.Store({
         deleteTask({commit}, id) {
             commit('deleteTask', id)
             commit('showSnackbar', 'Task deleted')
+        },
+        updateTaskTitle({commit}, payload) {
+            commit('updateTaskTitle', payload)
+            commit('showSnackbar', 'Task updated')
         }
     },
     getters: {},
